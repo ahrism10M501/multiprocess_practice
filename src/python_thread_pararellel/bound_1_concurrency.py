55b6a4e32b9c6ded6c50e547726b29defbb94231
ahrism10M501 <ariofeem@gmail.com>
2025-09-05 17:14:11 +0900

    file struct change

---
+++ src/python_thread_pararellel/bound_1_concurrency.py
@@ -0,0 +1,18 @@
+'''
+    Section 3
+    COncurrency, CPU Bound, I/O Bound - what is Concurrency
+    Keyword = Concurrency
+
+'''
+'''
+
+    Concurrency (동시성)
+        - CPU 가용성을 극대화 하기 위해 Parralleism의 단점 및 어려움을 소프트웨어 레벨에서 해결하기 위한 방법이다.
+        - 싱글코어에서 멀티스레드 패턴으로 작업을 처리
+        - 동시 작업에 있어 일정량 처리 후 다음 작업으로 넘기는 방식
+        - 제어권을 주고 받으며 작업을 처리하는 패턴. 병렬적이진 않으나 유사함
+        
+    Concurrency vs Parralleism
+        - 논리적 vs 
+
+'''
